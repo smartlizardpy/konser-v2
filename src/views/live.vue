252b4456d9e39aee4d821e93a136fd6a235665ae
smartlizardpy <154686525+smartlizardpy@users.noreply.github.com>
2025-01-12 16:51:29 +0300

    Update live.vue

--- src/views/live.vue
+++ src/views/live.vue
@@ -6,7 +6,7 @@
     </div>
     <h1 class="center text">Canlı Yayın</h1>
     <div class="video">
-        <iframe width="560" height="315" src="https://www.youtube.com/embed/hTHn4jUC_iw?si=urwH7ga2BrSlEqg_"
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/HbwMPNzLUyY?si=SXN5bQkfKrXWktFk"
             title="YouTube video player" frameborder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
             referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
