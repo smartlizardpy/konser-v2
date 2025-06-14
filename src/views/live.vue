6e445eb9d75c42bb44419dc50d3a765261e19907
smartlizardpy <ozan.kaygusuz11@gmail.com>
2025-06-14 09:01:55 +0300

    Update YouTube video source in live.vue

--- src/views/live.vue
+++ src/views/live.vue
@@ -6,7 +6,7 @@
     </div>
     <h1 class="center text">Canlı Yayın</h1>
     <div class="video">
-        <iframe width="560" height="315" src="https://www.youtube.com/embed/HbwMPNzLUyY?si=SXN5bQkfKrXWktFk"
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/D1kIoqhwVdk"
             title="YouTube video player" frameborder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
             referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
